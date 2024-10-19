265114c9273d0b2a66a8d938936e389bf6ff5458
Vinicius Ogawa <vini_ogawa@hotmail.com>
2024-10-19 17:29:37 -0300

    feat: create views for Users app

--- users/views.py
+++ users/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from rest_framework import generics
+from rest_framework.permissions import IsAuthenticated
+from rest_framework_simplejwt.authentication import JWTAuthentication
+from .models import User
+from .serializers import UserSerializer
+from .permissions import IsAdminOrOwner
 
-# Create your views here.
+
+class UserListCreateAPIView(generics.ListCreateAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+
+class UserRetrieveUpdateDestroyAPIView(generics.RetrieveUpdateDestroyAPIView):
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [IsAuthenticated, IsAdminOrOwner]
+
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
