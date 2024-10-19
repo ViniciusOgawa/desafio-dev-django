7981842c6a71aa4c924c72013b9f6c58520730c2
Vinicius Ogawa <vini_ogawa@hotmail.com>
2024-10-19 17:29:12 -0300

    feat: create permission for Users app

---
+++ users/permissions.py
@@ -0,0 +1,10 @@
+from rest_framework import permissions
+
+
+class IsAdminOrOwner(permissions.BasePermission):
+
+    def has_object_permission(self, request, view, obj):
+        if request.user.is_admin:
+            return True
+
+        return obj == request.user
