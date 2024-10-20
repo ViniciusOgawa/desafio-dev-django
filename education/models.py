fdaf379e10a4e56b0c95c2f69e0ef619595abc5f
Vinicius Ogawa <vini_ogawa@hotmail.com>
2024-10-19 22:45:00 -0300

    feat: create model for Education app

--- education/models.py
+++ education/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class Education(models.Model):
+    user = models.ForeignKey("users.User", on_delete=models.CASCADE, related_name='educations')
+    institution = models.CharField(max_length=100)
+    course = models.CharField(max_length=100)
+    ongoing = models.BooleanField(default=False)
+    period = models.CharField(max_length=50)
+
+    def __str__(self):
+        return f"{self.user.username} - {self.course} at {self.institution}"
