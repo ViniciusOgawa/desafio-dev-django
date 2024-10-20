ac593ba920a49aeca4e130dd80e13efb525a087b
Vinicius Ogawa <vini_ogawa@hotmail.com>
2024-10-19 22:22:10 -0300

    feat: create views for Contacts app

--- contacts/views.py
+++ contacts/views.py
@@ -1,3 +1,22 @@
-from django.shortcuts import render
+from rest_framework import generics
+from rest_framework.permissions import IsAuthenticated
+from rest_framework_simplejwt.authentication import JWTAuthentication
+from .models import Contact
+from .serializers import ContactSerializer
+from .permissions import IsAdminOrOwner
 
-# Create your views here.
+
+class ContactListCreateAPIView(generics.ListCreateAPIView):
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [IsAuthenticated]
+
+    queryset = Contact.objects.all()
+    serializer_class = ContactSerializer
+
+
+class ContactRetrieveUpdateDestroyAPIView(generics.RetrieveUpdateDestroyAPIView):
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [IsAuthenticated, IsAdminOrOwner]
+
+    queryset = Contact.objects.all()
+    serializer_class = ContactSerializer
