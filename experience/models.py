4762e7e9bd4c10e8e752b6d3d9ea2b9abc9b88c4
Vinicius Ogawa <vini_ogawa@hotmail.com>
2024-10-19 23:02:15 -0300

    feat: create model for Experience app

--- experience/models.py
+++ experience/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Experience(models.Model):
+    user = models.ForeignKey("users.User", on_delete=models.CASCADE, related_name='experiences')
+    role = models.CharField(max_length=100)
+    company = models.CharField(max_length=100)
+    period = models.CharField(max_length=50)
+    description = models.TextField()
+
+    def __str__(self):
+        return f"{self.user.username} - {self.role} at {self.company}"
