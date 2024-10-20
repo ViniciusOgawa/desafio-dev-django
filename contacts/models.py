12c8960c28355568ea4484e626a8d354bdb39f0c
Vinicius Ogawa <vini_ogawa@hotmail.com>
2024-10-19 22:21:15 -0300

    feat: create model for Contacts app

--- contacts/models.py
+++ contacts/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
+class Contact(models.Model):
+    user = models.ForeignKey("users.User", on_delete=models.CASCADE, related_name='contacts')
+    phone_number = models.CharField(max_length=15)
+    city = models.CharField(max_length=100)
+    state = models.CharField(max_length=100)
+    street = models.CharField(max_length=100)
+    number = models.CharField(max_length=10)
+    neighborhood = models.CharField(max_length=100)
+    postal_code = models.CharField(max_length=20)
+
+    def __str__(self):
+        return f"{self.user.username} - {self.phone_number}"
