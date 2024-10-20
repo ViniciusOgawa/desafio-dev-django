e7b059d1ce082e302095c02a29bbea15aff0070a
Vinicius Ogawa <vini_ogawa@hotmail.com>
2024-10-19 23:27:12 -0300

    feat: add new routes for user education, experience, and contacts retrieval

--- users/urls.py
+++ users/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import UserListCreateAPIView, UserRetrieveUpdateDestroyAPIView
+from .views import UserListCreateAPIView, UserRetrieveUpdateDestroyAPIView, UserEducationListAPIView, UserExperienceListAPIView, UserContactListAPIView
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 urlpatterns = [
@@ -7,4 +7,7 @@ urlpatterns = [
     path('users/<int:pk>/', UserRetrieveUpdateDestroyAPIView.as_view(), name='user-retrieve-update-destroy'),
     path("login/", TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path("refresh/", TokenRefreshView.as_view(), name='token_refresh'),
+    path('users/education/', UserEducationListAPIView.as_view(), name='user-education-list'),
+    path('users/experience/', UserExperienceListAPIView.as_view(), name='user-experience-list'),
+    path('users/contacts/', UserContactListAPIView.as_view(), name='user-contact-list'),
 ]
--- users/views.py
+++ users/views.py
@@ -4,6 +4,12 @@ from rest_framework_simplejwt.authentication import JWTAuthentication
 from .models import User
 from .serializers import UserSerializer
 from .permissions import IsAdminOrOwner
+from experience.serializers import ExperienceSerializer
+from education.serializers import EducationSerializer
+from contacts.serializers import ContactSerializer
+from experience.models import Experience
+from education.models import Education
+from contacts.models import Contact
 
 
 class UserListCreateAPIView(generics.ListCreateAPIView):
@@ -17,3 +23,27 @@ class UserRetrieveUpdateDestroyAPIView(generics.RetrieveUpdateDestroyAPIView):
 
     queryset = User.objects.all()
     serializer_class = UserSerializer
+
+class UserEducationListAPIView(generics.ListAPIView):
+    serializer_class = EducationSerializer
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        return Education.objects.filter(user=self.request.user).all()
+
+class UserExperienceListAPIView(generics.ListAPIView):
+    serializer_class = ExperienceSerializer
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        return Experience.objects.filter(user=self.request.user).all()
+
+class UserContactListAPIView(generics.ListAPIView):
+    serializer_class = ContactSerializer
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [IsAuthenticated]
+
+    def get_queryset(self):
+        return Contact.objects.filter(user=self.request.user)
