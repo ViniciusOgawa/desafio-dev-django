52fc2f0f62660b839e01b6055793dc1968c091d4
Vinicius Ogawa <vini_ogawa@hotmail.com>
2024-10-19 23:02:53 -0300

    feat: create views for Experience app

--- experience/views.py
+++ experience/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from rest_framework import generics
+from rest_framework.permissions import IsAuthenticated
+from rest_framework_simplejwt.authentication import JWTAuthentication
+from .models import Experience
+from .serializers import ExperienceSerializer
+from .permissions import IsAdminOrOwner
 
-# Create your views here.
+
+class ExperienceListCreateAPIView(generics.ListCreateAPIView):
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [IsAuthenticated]
+
+    queryset = Experience.objects.all()
+    serializer_class = ExperienceSerializer
+
+
+class ExperienceRetrieveUpdateDestroyAPIView(generics.RetrieveUpdateDestroyAPIView):
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [IsAuthenticated, IsAdminOrOwner]
+
+    queryset = Experience.objects.all()
+    serializer_class = ExperienceSerializer
+
