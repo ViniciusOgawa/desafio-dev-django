2de08a64f6d63de9cd78aad9c461cc1352783f3d
Vinicius Ogawa <vini_ogawa@hotmail.com>
2024-10-21 16:33:56 -0300

    feat: superuser creation

--- contacts/permissions.py
+++ contacts/permissions.py
@@ -4,6 +4,6 @@ from rest_framework import permissions
 class IsAdminOrOwner(permissions.BasePermission):
 
     def has_object_permission(self, request, view, obj):
-        if request.user.is_admin:
+        if request.user.is_superuser:
             return True
         return obj.user == request.user
--- education/permissions.py
+++ education/permissions.py
@@ -3,7 +3,7 @@ from rest_framework import permissions
 
 class IsAdminOrOwner(permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
-        if request.user.is_admin:
+        if request.user.is_superuser:
             return True
 
         return obj.user == request.user
