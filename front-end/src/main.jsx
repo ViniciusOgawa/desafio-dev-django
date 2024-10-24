7a3290398302523865d4d8f5e9d79cb3bea15c62
Vinicius Ogawa <vini_ogawa@hotmail.com>
2024-10-24 15:20:25 -0300

    feat: configure React context

--- front-end/src/main.jsx
+++ front-end/src/main.jsx
@@ -4,12 +4,15 @@ import { App } from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
 import { ChakraProvider } from "@chakra-ui/react";
 import { theme } from "./styles/Theme.js";
+import { UserProvider } from "./providers/UserContext.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <ChakraProvider theme={theme}>
     <React.StrictMode>
       <BrowserRouter>
-        <App />
+        <UserProvider>
+          <App />
+        </UserProvider>
       </BrowserRouter>
     </React.StrictMode>
   </ChakraProvider>
