ddf5a02ef8b5ac4223b3fbb34d5d662be88564c9
Vinicius Ogawa <vini_ogawa@hotmail.com>
2024-10-24 15:18:21 -0300

    feat: add CORS

--- back-end/CVManager/settings.py
+++ back-end/CVManager/settings.py
@@ -46,10 +46,12 @@ INSTALLED_APPS = [
     "experience",
     "users",
     "contacts",
+    'corsheaders',
 ]
 
 CORS_ALLOWED_ORIGINS = [
     'http://127.0.0.1:5173',
+    "http://localhost:5173",
 ]
 
 CORS_ALLOW_CREDENTIALS = True
@@ -57,6 +59,7 @@ CORS_ALLOW_CREDENTIALS = True
 CORS_ALLOW_ALL_ORIGINS = True
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
