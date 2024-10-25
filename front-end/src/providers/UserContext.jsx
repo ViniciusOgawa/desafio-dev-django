0e1a13c37561f40fb7151b34273d961755f95a2f
Vinicius Ogawa <vini_ogawa@hotmail.com>
2024-10-25 14:37:50 -0300

    feat: add user login functionality to userContext

--- front-end/src/providers/UserContext.jsx
+++ front-end/src/providers/UserContext.jsx
@@ -11,6 +11,36 @@ export const UserProvider = ({ children }) => {
   const navigate = useNavigate();
   const toast = useToast();
 
+  const [loadingLogin, setLoadingLogin] = useState(false);
+
+  const userLogin = async (formData) => {
+    try {
+      setLoadingLogin(true);
+      const response = await api.post("/api/login/", formData);
+      window.localStorage.clear();
+      window.localStorage.setItem("@TOKEN", response.data.access);
+      toast({
+        title: "Login efetuado com sucesso!",
+        position: "top-right",
+        status: "success",
+        duration: 5000,
+        isClosable: true,
+      });
+      navigate("/home");
+    } catch (error) {
+      toast({
+        title: "Senha ou email incorretos!",
+        position: "top-right",
+        status: "error",
+        duration: 5000,
+        isClosable: true,
+      });
+      console.log(error);
+    } finally {
+      setLoadingLogin(false);
+    }
+  };
+
   const [loadingRegister, setLoadingRegister] = useState(false);
 
   const userRegister = async (formData) => {
@@ -44,6 +74,8 @@ export const UserProvider = ({ children }) => {
       value={{
         userRegister,
         loadingRegister,
+        loadingLogin,
+        userLogin,
       }}
     >
       {children}
